995880ecc30d1eb388bc1ccb74f8007f702a1960
jrbraaten <braa4554@stthomas.edu>
2015-04-21 19:24:54 -0500

    Resource and ResourceManager Implementation
    Partially implemented Resource and ResourceManager classes.

--- Networking-Final/src/p2p_Final_Project/Resource.java
+++ Networking-Final/src/p2p_Final_Project/Resource.java
@@ -1,5 +1,39 @@
 package p2p_Final_Project;
 
-public class Resource {
+import java.io.File;
 
+public class Resource {
+	private String description;
+	private File location;
+	private String mimeType;
+	private ID resourceID;
+	
+	public Resource(ID id,String data)
+	{
+		
+	}
+	public Resource(ID id, String data,char delimeter)
+	{
+		
+	}
+	public String getDescription() {
+		return this.description;
+	}
+	public File getLocation() {
+		return this.location;
+	}
+	public String getMimeType() {
+		return this.mimeType;
+	}
+	public ID getResourceID() {
+		return this.resourceID;
+	}
+	public long getSizeInBytes()
+	{
+		return 0;
+	}
+	public boolean matches(String searchString)
+	{
+		return false;
+	}
 }
--- Networking-Final/src/p2p_Final_Project/ResourceManager.java
+++ Networking-Final/src/p2p_Final_Project/ResourceManager.java
@@ -1,16 +1,34 @@
 package p2p_Final_Project;
 
+import java.io.File;
+import java.util.Hashtable;
 import java.util.Map;
 
+//class is a singleton
 public class ResourceManager {
 	private Map<ID,Resource> resourceDirectory;
-	private static ResourceManager resourceManger = new ResourceManager();
+	private static ResourceManager resourceManager = new ResourceManager();
 	
 	public ResourceManager()
 	{
+		this.resourceDirectory = new Hashtable<ID,Resource>();
+	}
+	public static ResourceManager getInstance()
+	{
+		return resourceManager;
+	}
+	public static ResourceManager newInstance()
+	{
+		return getInstance();
+	}
+	public static void loadResourcesFrom(File file)
+	{
 		
 	}
-
+	public Resource[] getResourcesThatMatch(String searchString)
+	{
+		return null;
+	}
 }
 
 
