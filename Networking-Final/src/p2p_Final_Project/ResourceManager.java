49f27b025bb3bcd567c808e49187766d39701f4c
jrbraaten <braa4554@stthomas.edu>
2015-04-21 19:23:40 -0500

    New Classes
    Added new classes from new UML

---
+++ Networking-Final/src/p2p_Final_Project/ResourceManager.java
@@ -0,0 +1,17 @@
+package p2p_Final_Project;
+
+import java.util.Map;
+
+public class ResourceManager {
+	private Map<ID,Resource> resourceDirectory;
+	private static ResourceManager resourceManger = new ResourceManager();
+	
+	public ResourceManager()
+	{
+		
+	}
+
+}
+
+
+
